48f1e36393b725e78fdae7e7e900983e13ed5528
Justin Hagerty <wheelsmanx@gmail.com>
2021-05-01 13:43:23 -0400

    added some documentation for global maps and shapefiles

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ import requests
 
 # I am planning to build this out to have a UI that has a map where you can see all of the nuclear plants on the globe and click on them and get info
 # I think this is a good idea to use QT as I have already used TK: https://www.qt.io/qt-for-python
-
+# I am going to start with this example/tutorial and then see if I can help the shapefile a bit to see if I can make it better:
+# https://towardsdatascience.com/a-complete-guide-to-an-interactive-geographical-map-using-python-f4c5197e23e0
 
 
 class NuclearInformation:
